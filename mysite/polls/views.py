f9426c8ee10b5f76588d3c2c7988d77bfeba2919
Gianik <paoduguran@gmail.com>
2022-10-18 02:13:52 +0800

    Writing your first Django app, part 5

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.http import Http404
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
@@ -11,14 +12,23 @@ from .models import Choice, Question
 #     return HttpResponse("Hello, world. You're at the polls index")
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
